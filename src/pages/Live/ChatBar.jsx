ebfeccbe6e673e8a263df69ca263b22218cc0d3c
BEYUD <beyud@attcloud.org>
2025-04-17 17:52:52 +0700

    build 17/04

--- src/pages/Live/ChatBar.jsx
+++ src/pages/Live/ChatBar.jsx
@@ -66,7 +66,7 @@ export default function ChatBar({ ...rest }) {
   const [isSendCode, setIsSendCode] = useState(false);
   const [message, setMessage] = useState("");
   const [isEmoOpen, setIsEmoOpen] = useState(false);
-  const [allowChat, setAllowChat] = useState(true);
+  const [allowChat, setAllowChat] = useState(false);
 
   const { isAuthenticated } = useAuth();
   const {
@@ -157,29 +157,29 @@ export default function ChatBar({ ...rest }) {
   }, [manualReconnect, currentHubConnection]);
 
   useEffect(() => {
-    // const interval = setInterval(() => {
-    //   if (
-    //     liveDetailData?.scheduleTime &&
-    //     dayjs().isAfter(
-    //       dayjs(liveDetailData.scheduleTime).subtract(10, "minute")
-    //     )
-    //   ) {
-    //     setAllowChat(true);
-    //   }
-    // }, 1000);
+    const interval = setInterval(() => {
+      if (
+        liveDetailData?.scheduleTime &&
+        dayjs().isAfter(
+          dayjs(liveDetailData.scheduleTime).subtract(10, "minute")
+        )
+      ) {
+        setAllowChat(true);
+      }
+    }, 1000);
 
-    // if (liveDetailData?.isStreaming) {
-    //   setAllowChat(true);
-    //   clearInterval(interval);
-    // }
+    if (liveDetailData?.isStreaming) {
+      setAllowChat(true);
+      clearInterval(interval);
+    }
 
-    // if (!liveDetailData?.isStreaming && !liveDetailData?.scheduleTime) {
-    //   setAllowChat(false);
-    //   clearInterval(interval);
-    // }
+    if (!liveDetailData?.isStreaming && !liveDetailData?.scheduleTime) {
+      setAllowChat(false);
+      clearInterval(interval);
+    }
 
     return () => {
-      // clearInterval(interval);
+      clearInterval(interval);
     };
   }, [liveDetailData, allowChat]);
 
