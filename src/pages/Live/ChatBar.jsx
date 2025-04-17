d5dc23d1e567545d354118da69a81b5445e4d364
BEYUD <beyud@attcloud.org>
2025-04-17 07:55:36 +0700

    update chat text color

--- src/pages/Live/ChatBar.jsx
+++ src/pages/Live/ChatBar.jsx
@@ -157,29 +157,29 @@ export default function ChatBar({ ...rest }) {
   }, [manualReconnect, currentHubConnection]);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (
-        liveDetailData?.scheduleTime &&
-        dayjs().isAfter(
-          dayjs(liveDetailData.scheduleTime).subtract(10, "minute")
-        )
-      ) {
-        setAllowChat(true);
-      }
-    }, 1000);
+    // const interval = setInterval(() => {
+    //   if (
+    //     liveDetailData?.scheduleTime &&
+    //     dayjs().isAfter(
+    //       dayjs(liveDetailData.scheduleTime).subtract(10, "minute")
+    //     )
+    //   ) {
+    //     setAllowChat(true);
+    //   }
+    // }, 1000);
 
-    if (liveDetailData?.isStreaming) {
-      setAllowChat(true);
-      clearInterval(interval);
-    }
+    // if (liveDetailData?.isStreaming) {
+    //   setAllowChat(true);
+    //   clearInterval(interval);
+    // }
 
-    if (!liveDetailData?.isStreaming && !liveDetailData?.scheduleTime) {
-      setAllowChat(false);
-      clearInterval(interval);
-    }
+    // if (!liveDetailData?.isStreaming && !liveDetailData?.scheduleTime) {
+    //   setAllowChat(false);
+    //   clearInterval(interval);
+    // }
 
     return () => {
-      clearInterval(interval);
+      // clearInterval(interval);
     };
   }, [liveDetailData, allowChat]);
 
